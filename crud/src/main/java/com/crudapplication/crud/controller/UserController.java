954b8446a511befd1b7987c286a53c2a62fb7353
rishashetty05 <rishashetty05@gmail.com>
2024-11-01 12:46:22 +0530

    FIRST Back End Commit

---
+++ crud/src/main/java/com/crudapplication/crud/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.crudapplication.crud.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@PostMapping("/user")
+	public User addUser () {
+		
+	}
+
+}
